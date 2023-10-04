8412151cbe24f7f92fd15d81d178b0081e355ff0
elishakaranja <145563815+elishakaranja@users.noreply.github.com>
2023-10-04 21:35:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,89 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceInFeet){
   // Write your code here!
+ if (distanceInFeet <= 400) 
+ return "This one is on me!"
+
+ else if (distanceInFeet > 2000 && distanceInFeet <= 2500)
+ return "I will gladly take your thirty bucks."
+
+ else 
+ return "No can do."
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC")
+  return "Ok, sounds good."
+else return "No go."
+ 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === "generous")
+return "Thank you so much."
+   else if (tip === "not as generous")
+   return "Thank you."
+  else return "Bye."
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
